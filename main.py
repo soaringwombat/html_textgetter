5ba344a51a46559ec520d83ba2bc3e431470e3fd
yosuke <yousuke.k2001@gmail.com>
2023-09-17 13:58:46 +0900

    :sparkles: [add] 空白と改行を含むオプションの追加

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 input_file_path = 'input.txt'
 output_file_path = 'output.txt'
 
+# オプション
+include_space = False   # 空白を含む
+include_line = False    # 改行を含むか(非推奨)
+
 flag = True         # 現在の位置をテキストの中か外かを判定するフラグ(True:テキストの中, False:テキストの外)
 text_flag = False   # 改行位置を決定するためのフラグ
 
@@ -8,8 +12,10 @@ with open(input_file_path, 'r', encoding='utf-8') as input_file:
     with open(output_file_path, 'w', encoding='utf-8') as output_file:
         while True:
             char = input_file.read(1)  # ファイルから1文字読み込む
+            # ファイルの終わりに達したらループを終了
             if not char:
-                break  # ファイルの終わりに達したらループを終了
+                break
+            # 文字の判定
             if char == '<':
                 flag = False
             elif char == '>':
@@ -18,9 +24,15 @@ with open(input_file_path, 'r', encoding='utf-8') as input_file:
                     text_flag = False
                 flag = True
             elif char == ' ':
-                pass
+                if include_space:
+                    output_file.write(char)
+                else:
+                    pass
             elif char == '\n':
-                pass
+                if include_line:
+                    output_file.write(char)
+                else:
+                    pass
             # テキストの中身を書き込む
             elif flag:
                 output_file.write(char)
